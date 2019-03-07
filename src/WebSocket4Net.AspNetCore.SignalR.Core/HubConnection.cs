9489c6d0c0998cbec583030613bc24e9f3c78837
丁松杰 <377973147@qq.com>
2019-03-07 12:40:03 +0800

    remove   unnecessary packages

--- src/WebSocket4Net.AspNetCore.SignalR.Core/HubConnection.cs
+++ src/WebSocket4Net.AspNetCore.SignalR.Core/HubConnection.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Connections;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json.Linq;
